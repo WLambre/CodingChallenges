4ca5e4e9b29abe82693a21e1f512b92936720432
WLambre <50972432+WLambre@users.noreply.github.com>
2019-10-02 12:53:44 +1000

    Create WLambre.py

---
+++ fizzbuzz/python/WLambre.py
@@ -0,0 +1,10 @@
+for x in range(1,101):
+    if x % 3 == 0:
+        if x % 5 == 0:
+            print ('FizzBuzz')
+        else:
+            print ('Fizz')
+    elif x % 5 ==0:
+        print('Buzz')
+    else:
+        print(x)
